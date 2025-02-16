344487056b072bd21c93492ecd19306d294b6d7d
abhinav1912 <abhinavmathur1912@gmail.com>
2025-02-16 15:45:27 +0530

    wip: added source and txnId to Transaction model

--- Bookkeeper/Transaction.swift
+++ Bookkeeper/Transaction.swift
@@ -5,19 +5,28 @@ import SwiftData
 
 @Model
 final class Transaction {
+    /// internal id used for ensuring uniqueness
     var id: UUID
+    /// external id issued by the bank for this txn
+    var transactionId: String?
     var timestamp: TimeInterval
     var amount: Double
     var type: TType
     var status: Status
+    /// the other account involved in this transaction
+    var source: String
 
     init(
+        source: String,
         amount: Double,
+        transactionId: String? = nil,
         type: TType = .debit,
         status: Status = .completed,
         timestamp: TimeInterval = Date().timeIntervalSince1970
     ) {
         self.id = UUID()
+        self.transactionId = transactionId
+        self.source = source
         self.amount = amount
         self.type = type
         self.status = status
@@ -42,6 +51,7 @@ extension Transaction {
 extension Transaction {
     static func getRandomTransaction() -> Transaction {
         return Transaction(
+            source: ["Bank", "Grocery", "Credit Card", "Cash"].randomElement() ?? "Bank",
             amount: Double.random(in: 0...1000),
             type: TType.allCases.randomElement() ?? .debit,
             status: Status.allCases.randomElement() ?? .completed
